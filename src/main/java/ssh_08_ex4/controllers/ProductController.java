452bd273171673e26f4fd2b93c8749dd968e3c69
osparking <jbpark03@gmail.com>
2023-09-23 22:53:47 +0900

    product list action method defined

--- src/main/java/ssh_08_ex4/controllers/ProductController.java
+++ src/main/java/ssh_08_ex4/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package ssh_08_ex4.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.AllArgsConstructor;
 import ssh_08_ex4.services.ProductService;
@@ -10,5 +12,13 @@ import ssh_08_ex4.services.ProductService;
 public class ProductController {
 	
 	private final ProductService productService;
+	
+	@GetMapping("/products")
+	public String products(Model backData) {
+		var products = productService.getProductList();
+		backData.addAttribute("products", products);
+		
+		return "products.html";
+	}
 	
 }
