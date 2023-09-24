580176e3a68a318571260e5c89a705ebd3d077c1
osparking <jbpark03@gmail.com>
2023-09-24 11:11:04 +0900

    errors of method and path fixed

--- src/main/java/ssh_08_ex4/controllers/ProductController.java
+++ src/main/java/ssh_08_ex4/controllers/ProductController.java
@@ -13,28 +13,28 @@ import ssh_08_ex4.services.ProductService;
 @Controller
 @AllArgsConstructor
 public class ProductController {
-	
+
 	private final ProductService productService;
-	
+
 	@GetMapping("/products")
 	public String products(Model backData) {
 		var products = productService.getProductList();
 		backData.addAttribute("products", products);
-		
+
 		return "products.html";
 	}
-	
-	@PostMapping("/add")
+
+	@PostMapping(path = "/products")
 	public String addProduct(@RequestParam String name,
 			@RequestParam Double price, Model model) {
 		var product = new Product();
 		product.setName(name);
 		product.setPrice(price);
 		productService.addProduct(product);
-		
+
 		model.addAttribute("products", productService.getProductList());
-		
+
 		return "products.html";
 	}
-	
+
 }
