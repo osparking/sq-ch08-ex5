fd86b510a7482cc024149a3b7dde304e895d8512
osparking <jbpark03@gmail.com>
2023-09-24 10:24:12 +0900

    add product action method added

--- src/main/java/ssh_08_ex4/controllers/ProductController.java
+++ src/main/java/ssh_08_ex4/controllers/ProductController.java
@@ -3,8 +3,13 @@ package ssh_08_ex4.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.AllArgsConstructor;
+import ssh_08_ex4.model.Product;
 import ssh_08_ex4.services.ProductService;
 
 @Controller
@@ -17,6 +22,19 @@ public class ProductController {
 	public String products(Model backData) {
 		var products = productService.getProductList();
 		backData.addAttribute("products", products);
+		
+		return "products.html";
+	}
+	
+	@RequestMapping(path = "/add", method = RequestMethod.POST)
+	public String addProduct(@RequestParam String name,
+			@RequestParam Double price, Model model) {
+		var product = new Product();
+		product.setName(name);
+		product.setPrice(price);
+		productService.addProduct(product);
+		
+		model.addAttribute("products", productService.getProductList());
 		
 		return "products.html";
 	}
